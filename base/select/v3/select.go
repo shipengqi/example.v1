0472fdd850257d29ab61ea1bfc8c69f54b460f80
shipengqi <pooky.shipengqi@gmail.com>
2021-10-20 11:23:56 +0800

    select

---
+++ base/select/v3/select.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func main() {
+	go sigReceiver("receiver1")
+	go sigReceiver("receiver2")
+	fmt.Println("block ...")
+	time.Sleep(time.Second * 10)
+}
+
+func sigReceiver(flag string) {
+	var once sync.Once
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+
+	fmt.Println("Receiving ...")
+	select {
+	case sig := <-quit:
+		once.Do(func() {
+			fmt.Printf("%s get a signal %s\n", flag, sig.String())
+		})
+		fmt.Println("Received ...")
+		return
+	}
+
+}
